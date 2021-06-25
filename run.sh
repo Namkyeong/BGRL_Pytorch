1834aac982ff800e3fab951c331f9e7beb208a1f
Namkyeong <namkyeong96@naver.com>
2021-06-25 15:32:12 +0900

    WikiCS data and experimental settings

--- run.sh
+++
@@ -1,6 +0,0 @@
-rm -rf data
-python train.py --device 0 --epochs 10000 --name WikiCS --aug_params 0.2 0.1 0.2 0.3 --lr 0.0005 --layers 512 256 --pred_hid 512
-python train.py --device 0 --epochs 10000 --name computers --aug_params 0.2 0.1 0.5 0.4 --lr 0.0005 --layers 256 128 --pred_hid 512
-python train.py --device 0 --epochs 10000 --name photo --aug_params 0.1 0.2 0.4 0.1 --lr 0.0001 --layers 512 256 --pred_hid 512
-python train.py --device 0 --epochs 10000 --name cs --aug_params 0.3 0.4 0.3 0.2 --lr 0.00001 --layers 512 256 --pred_hid 512
-python train.py --device 0 --epochs 10000 --name physics --aug_params 0.1 0.4 0.4 0.1 --lr 0.00001 --layers 256 128 --pred_hid 512
